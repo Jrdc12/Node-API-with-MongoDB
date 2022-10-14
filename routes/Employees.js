f897d23b892397ca4fdcd990f4f336f7f6c695e3
Jrdc12 <juliodc289@gmail.com>
2022-10-14 08:09:28 -0400

    Made employees fully functional

--- routes/Employees.js
+++ routes/Employees.js
@@ -21,6 +21,8 @@ router.post('/emp/employees', async (req, res) => {
 })
 
 // Get Method
+
+// this gets all employees
 router.get('/emp/employees', async (req, res) => {
     try {
         const employees = await Employee.find()
@@ -30,6 +32,52 @@ router.get('/emp/employees', async (req, res) => {
     }
 })
 
+router.get('/emp/employees/:id', async (req, res) => {
+    try {
+        const employee = req.body
+        const employeeId = await Employee.findById(req.params.id)
+        res.status(200).json(employeeId)
+    }
+    catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
+// PUT Method
+router.put('/emp/employees/:id', async (req, res) => {
+    try {
+        const employee = await Employee.findById(req.params.id)
+        if (req.body.first_name != null) {
+            employee.first_name = req.body.first_name
+        }
+        if (req.body.last_name != null) {
+            employee.last_name = req.body.last_name
+        }
+        if (req.body.email != null) {
+            employee.email = req.body.email
+        }
+        if (req.body.gender != null) {
+            employee.gender = req.body.gender
+        }
+        if (req.body.salary != null) {
+            employee.salary = req.body.salary
+        }
+        const updatedEmployee = await employee.save()
+        res.status(200).json(updatedEmployee)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
+
+// Delete Method
+router.delete('/emp/employees/:id', async (req, res) => {
+    try {
+        const employee = await Employee.findById(req.params.id)
+        const deletedEmployee = await employee.remove()
+        res.status(200).json({message: "Employee Deleted"})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
 module.exports = router
